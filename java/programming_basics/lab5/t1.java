4c99846aac8feec553dd266169a87874ba42e51a
johnil <ssushnost@gmail.com>
2022-11-22 16:13:44 +0300

    refactor: rename file

---
+++ java/programming_basics/lab5/t1.java
@@ -0,0 +1,32 @@
+import java.util.InputMihException;
+import java.util.Scanner;
+
+public class t1 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int read_value;
+    int zero_index = -1;
+    int iterator = 0;
+    while (true) {
+      try {
+        read_value = in.nextInt();
+        if (zero_index == -1) {
+          if (read_value == 0) {
+            zero_index = iterator;
+            System.out.println(zero_index + 1);
+          } else {
+            System.out.println(0);
+          }
+        } else {
+          System.out.println(zero_index + 1);
+        }
+
+      } catch (InputMismatchException e) {
+        System.out.println("wrong input");
+        break;
+      } finally {
+      }
+      iterator++;
+    }
+  }
+}
