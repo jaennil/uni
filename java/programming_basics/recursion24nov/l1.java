93baeb02a112f8cabfd113f4ee837a176eca4a0f
johnil <ssushnost@gmail.com>
2022-11-26 17:32:16 +0300

    refactor: format

--- java/programming_basics/recursion24nov/l1.java
+++ java/programming_basics/recursion24nov/l1.java
@@ -10,24 +10,25 @@ public class l1 {
     System.out.println(fib(8));
   }
 
-  public static int factorial(int number){
+  public static int factorial(int number) {
     int start = 1;
-    for (int i = 2; i < number+1; i++) {
-      start*=i;
+    for (int i = 2; i < number + 1; i++) {
+      start *= i;
     }
 
     return start;
   }
 
-  public static int rfactorial(int number){
-    return number == 0 ? 1 : number*rfactorial(number-1);
+  public static int rfactorial(int number) {
+    return number == 0 ? 1 : number * rfactorial(number - 1);
   }
 
-  public static int fib (int number){
-    if (number == 0) return 0;
-    if (number == 1) return 1;
-    return fib(number-2)+fib(number-1);
+  public static int fib(int number) {
+    if (number == 0)
+      return 0;
+    if (number == 1)
+      return 1;
+    return fib(number - 2) + fib(number - 1);
   }
-  
+
 }
-
--- java/programming_basics/recursion24nov/l2.java
+++ java/programming_basics/recursion24nov/l2.java
@@ -1,41 +1,45 @@
 import java.util.Scanner;
+
 public class l2 {
     static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println(rpow(2,10));
+        System.out.println(rpow(2, 10));
     }
-    
-    public static int pow(int number, int power){
+
+    public static int pow(int number, int power) {
         int res = 1;
-        while (power > 0){
-            if ((power & 1) != 0){
-                res*=number;
+        while (power > 0) {
+            if ((power & 1) != 0) {
+                res *= number;
             }
             power >>= 1;
-            number*=number;
+            number *= number;
         }
         return res;
     }
-    
-    public static int rpow(int number, int power){
-        if (power == 0) return 1;
-        if ((power&1) != 0) return number*rpow(number*number, power>>1);
-        return rpow(number*number, power>>1);
+
+    public static int rpow(int number, int power) {
+        if (power == 0)
+            return 1;
+        if ((power & 1) != 0)
+            return number * rpow(number * number, power >> 1);
+        return rpow(number * number, power >> 1);
     }
-    
-    public static String bin(int number){
+
+    public static String bin(int number) {
         String res = "";
-        while (number > 0){
-            res+=String.valueOf(number%2);
-            number/=2;
+        while (number > 0) {
+            res += String.valueOf(number % 2);
+            number /= 2;
         }
         return reverse(res);
     }
-    
-    public static String reverse(String str){
+
+    public static String reverse(String str) {
         String res = "";
         for (int i = 0; i < str.length(); i++) {
-            res+=str.charAt(str.length()-i-1);
+            res += str.charAt(str.length() - i - 1);
         }
         return res;
     }
