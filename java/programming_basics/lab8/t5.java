d0f3322a21f081487faff2969b6cb115105373cb
johnil <ssushnost@gmail.com>
2022-11-26 17:30:35 +0300

    feat: string concat using array

--- java/programming_basics/lab8/t5.java
+++ java/programming_basics/lab8/t5.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
  * t5
  */
 public class t5 {
-    static Scanner scan = new Scanner(System.in);
+  static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.print("enter first string: ");
-        String str1 = scan.nextLine();
-        System.out.print("etner second string: ");
-        String str2 = scan.nextLine();
+  public static void main(String[] args) {
+    System.out.print("enter first string: ");
+    String str1 = scan.nextLine();
+    System.out.print("etner second string: ");
+    String str2 = scan.nextLine();
 
-        System.out.print("method result: " + concat(str1, str2));
+    System.out.println("method result: " + concat(str1, str2));
 
-        scan.close();
+    scan.close();
+  }
+
+  public static String concat(String str1, String str2) {
+    char[] array = new char[str1.length() + str2.length()];
+    for (int i = 0; i < str1.length(); i++) {
+      array[i]=str1.charAt(i);
     }
-
-    public static String concat(String str1, String str2) {
-        return str1 + str2;
+    for (int i = str1.length(), cnt = 0; i < str1.length() + str2.length(); i++, cnt++) {
+      array[i] = str2.charAt(cnt);
     }
+    return String.valueOf(array);
+  }
 }
