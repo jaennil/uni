7ab6e1af5347d4e3c91a8710b0f5054b9e64b0e6
johnil <ssushnost@gmail.com>
2022-12-11 20:28:02 +0300

    feat: task 5

--- python/testing_basics/labs/lab1/4.py
+++ python/testing_basics/labs/lab1/4.py
@@ -1,24 +1,31 @@
-inp = input()
-res = ""
-separators = ["-", " "]
-a = 0
-for index, char in enumerate(inp):
-    if char in separators:
-        word = inp[a:index]
-        print(f"{word = }")
-        a = index+1
-        l = len(word)
-        if l:
-            if l % 2 == 0:
-                print(f"{word[:l//2] = }")
-                print(f"{word[l//2] = }")
-                print(f"{word[l//2:] = }")
-                word = word[:l//2] + chr(ord(word[0]) + 1) + word[l//2:]
-            else:
-                word = word[:l//2] + word[l//2+1:]
-            res += word
-        else:
-            res += char
+# Пользователь вводит строку из произвольного числа символов. В середину каждого слова (если четное число символов) добавить соседний справа символ из таблицы кодировки для первого символа слова; если нечетное количество символов в слове, то требуется удалить центральный символ. Словом считается конструкция, отделенная знаками препинания, пробелами или служебными символами.
 
-        print(f"{word = }")
-print(f"{res = }")
+string = input("enter string: ")
+punctuation = (",", ".", "!", "?", ":", ";", " ", "/")
+
+
+def change_word(word):
+    if len(word) % 2 == 0:
+        return word[: len(word) // 2] + chr(ord(word[0]) + 1) + word[len(word) // 2:]
+    return word[: len(word) // 2] + word[len(word) // 2 + 1:]
+
+
+word = ""
+newstring = ""
+flag = False
+for index, symbol in enumerate(string):
+    if symbol in punctuation:
+        flag = True
+        if not word:
+            newstring += symbol
+            continue
+        changed_word = change_word(word)
+        newstring += changed_word + symbol
+        word = ""
+    else:
+        flag = False
+        word += symbol
+if word:
+    changed_word = change_word(word)
+    newstring += changed_word
+print(f'{newstring = }')
